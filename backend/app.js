ea8804cd9138b0a819c32825479b7084b1b2b52f
Frex6s <lucicris612@gmail.com>
2026-06-01 10:19:00 +0200

    Table of the 12 books on the homepage

--- backend/app.js
+++ backend/app.js
@@ -3,22 +3,100 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('Requete ok');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();    
-});
-
-app.use((req, res, next) => {
-    res.json({ message: 'bien reçu' });
-    next();
-});
-
-app.use((req, res) => {
-    console.log('Réponse envoyé bien');
+app.use('/api/books', (req, res, next) => {
+  const books = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Milwaukee Mission',
+      description: 'Les infos de mon premier objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeoi',
+      title: 'Milwaukee Mission',
+      description: 'Les infos de mon premier objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeoi',
+      title: 'Milwaukee Mission',
+      description: 'Les infos de mon premier objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeoi',
+      title: 'Milwaukee Mission',
+      description: 'Les infos de mon premier objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: '/images/Milwaukee_Mission.jpg',
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(books);
 });
 
 module.exports = app;
