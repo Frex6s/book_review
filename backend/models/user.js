49221837810507839681211c45dd2a0fa37f2632
Frex6s <lucicris612@gmail.com>
2026-06-01 15:05:41 +0200

    login add

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true }
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
-userSchema.plugin(uniqueValidator);
+const pluginToUse = uniqueValidator.default || uniqueValidator;
+userSchema.plugin(pluginToUse);
 
 module.exports = mongoose.model('User', userSchema);
